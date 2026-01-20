a0ab80bf2bd6accb1b07d27a39c80a74f9a6035c
thissidemayur <thissidemayur@gmail.com>
2026-01-20 22:09:41 +0530

    feat(types): add types for all DAL(data access layer) function

---
+++ src/types/database.ts
@@ -0,0 +1,67 @@
+export type iBlog = {
+  id: number;
+  title: string;
+  slug: string;
+  content: string;
+  published_at: Date;
+  category: BlogType;
+  summary: string;
+  image_url: string;
+  is_featured: boolean;
+};
+export type BlogType = "TECHNICAL" | "NON_TECHNICAL";
+
+export type IMsg = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+
+export type iProject = {
+    id: number;
+    title: string;
+    content: string;
+    summary: string;
+    image_url: string;
+    live_url?: string;
+    repo_url: string;
+    slug: string;
+    is_featured: boolean;
+    problem_statement: string;
+    solution_approach: string;
+    key_learnings: string[];
+    challenges_faced: string[];
+    created_at: Date;
+    tech_stack: string[];
+};
+
+
+export type iResume = {
+  id: number;
+  version_name: string;
+  focus_area: ResumeType
+  file_url: string;
+  is_latest: boolean;
+  created_at: string;
+};
+export type ResumeType = "fullstack" | "backend" | "devops";
+
+
+export type iTech = {
+  id: number;
+  name: string;
+  category: TechCategory;
+  icon_slug: string;
+  is_main_stack: boolean;
+};
+export type TechCategory =
+  | "languages & runtimes"
+  | "frontend"
+  | "backend"
+  | "database & ORMs"
+  | "devops"
+  | "tools"
+  | "other";
+
