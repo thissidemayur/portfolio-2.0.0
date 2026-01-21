7d526615e8889965a4e711f4bcd0cf47f21d7ccc
thissidemayur <thissidemayur@gmail.com>
2026-01-21 22:58:41 +0530

    feat(dal): add database opereations on certifications

---
+++ src/dal/certificates.dal.ts
@@ -0,0 +1,78 @@
+import { query } from "@/lib/db";
+import { iCertificate } from "@/types/database";
+
+
+export async function getAllCertificates(): Promise<iCertificate[]> {
+  try {
+    const certs = await query(
+      "SELECT * FROM certificates ORDER BY issue_date DESC",
+    );
+    return certs.rows;
+  } catch (error) {
+    console.error("Error fetching all certificates:", error);
+    return [];
+  }
+}
+
+
+export async function getFeaturedCertificates(): Promise<iCertificate[]> {
+  try {
+    const certs = await query(
+      "SELECT * FROM certificates WHERE show_on_home = true OR is_industry_standard = true ORDER BY issue_date DESC LIMIT 4",
+    );
+    return certs.rows;
+  } catch (error) {
+    console.error("Error fetching featured certificates:", error);
+    return [];
+  }
+}
+
+
+
+export async function createCertificate(data: Omit<iCertificate, 'id'>) {
+  try {
+    const sql = `
+      INSERT INTO certificates (
+        title, issuer, issue_date, expiry_date, image_url, 
+        verify_link, slug, credential_url, is_industry_standard, show_on_home
+      ) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+      RETURNING *;
+    `;
+    const values = [
+      data.title,
+      data.issuer,
+      data.issue_date,
+      data.expiry_date,
+      data.image_url,
+      data.verify_link,
+      data.slug,
+      data.credential_url,
+      data.is_industry_standard,
+      data.show_on_home // The extra field for Home visibility
+    ];
+
+    const result = await query(sql, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error creating certificate:", error);
+    throw new Error("Failed to create certificate");
+  }
+}
+
+
+export async function deleteCertificate(id: number) {
+  try {
+    const sql = "DELETE FROM certificates WHERE id = $1 RETURNING id;";
+    const result = await query(sql, [id]);
+    
+    if (result.rowCount === 0) {
+      throw new Error("Certificate not found");
+    }
+    
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error deleting certificate:", error);
+    throw new Error("Failed to delete certificate");
+  }
+}
