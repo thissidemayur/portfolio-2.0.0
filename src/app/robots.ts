30ad6059a1b7095d2bd54ade1e05d28c9fe706f6
thissidemayur <thissidemayur@gmail.com>
2026-01-24 15:23:54 +0530

    feat(SEO): add robot.txt file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin","/admin/*"],
+      disallow: ["app/admin","app/admin/*"],
     },
     sitemap: "'https://thissidemayur.me/sitemap.xml",
   };
