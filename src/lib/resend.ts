fd87769fa60bdf95e72e23e731f2cb3abbfd6f0d
thissidemayur <thissidemayur@gmail.com>
2026-01-24 15:35:48 +0530

    feat(resend): add resend service to send email to email address

---
+++ src/lib/resend.ts
@@ -0,0 +1,8 @@
+import { Resend } from "resend";
+
+if(!process.env.RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY is missing from environment variables");
+}
+
+
+export const resend = new Resend(process.env.RESEND_API_KEY);
