3d4881d198a148376d35ab06010515a86ff8da03
thissidemayur <thissidemayur@gmail.com>
2026-01-27 19:29:01 +0530

    do later

---
+++ src/app/preload-resources.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import ReactDOM from "react-dom";
+
+export function PreloadResources() {
+
+    // ReactDOM.preconnect("",{crossOrigin:"anonymous"})
+    // ReactDOM.preconnect("https://fonts.gstatic.com", {
+    //   crossOrigin: "anonymous",
+    // });
+
+    // ReactDOM.preload("/logo.svg", { as: "image" });
+
+    // ReactDOM.prefetchDNS("https://www.google-analytics.com");
+    // ReactDOM.prefetchDNS("https://www.googletagmanager.com");
+    
+    return "..."
+}
