1b35c0afe8c393db7d36625e1d71f91b12b27904
thissidemayur <thissidemayur@gmail.com>
2026-01-27 18:55:02 +0530

    feat(page): add dynamic import to reduce LCP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+import dynamic from "next/dynamic";
 import Hero from "@/components/home/Hero";
 import ActiveBentoGrid from "@/components/home/ActiveBentoGrid";
-import LogicPhilosophy from "@/components/home/LogicPhilosophy";
-import StackArchitecture from "@/components/home/StackArchitecture";
-import { GithubSection } from "@/components/home/GithubSection";
-import CertificationVault from "@/components/home/CertfificationVault";
-import FeaturedProjects from "@/components/home/FeaturedProjects";
-import RecentBlogs from "@/components/home/RecentBlogs";
+
+
+import { ProjectSkeleton } from "@/components/home/Skelton";
+import BlogsLoading from "@/components/admin/SuspenseBlog";
+import CertsLoading from "@/components/admin/SuspenseCertificates";
+import {
+  LogicPhilosophy,
+  GithubSection,
+  StackArchitecture
+} from "@/components/home/DynamicClientComponont";
+
+
+const FeaturedProjects = dynamic(() =>import( "@/components/home/FeaturedProjects"),{
+  ssr:true,
+  loading:()=><ProjectSkeleton/>
+});
+
+const RecentBlogs = dynamic(() => import("@/components/home/RecentBlogs"),{
+  ssr:true,
+  loading:()=><BlogsLoading />
+});
+
+const CertificationVault = dynamic(
+  () => import("@/components/home/CertfificationVault"),
+  {
+    ssr: true,
+    loading: () => <CertsLoading />,
+  },
+);
+
+
 export default function Page() {
   return (
     <main>
